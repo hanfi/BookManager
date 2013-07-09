1f50179f591bc17e5aa9e0b13d06e1699d777a9f
hanfi <souhail.hanfi@neoxia.com>
2013-07-09 15:12:33 +0200

    redesign of the service

--- app/controllers/booksController.php
+++ app/controllers/booksController.php
@@ -15,11 +15,15 @@ class booksController extends \BaseController {
         $response = [];
         foreach($books as $book){
             $httpResponse = \Httpful\Request::get($url.$book->isbn)->send();
-            $push = ["book" => $book, "detail" => $httpResponse->body->items];
+            $resp= $httpResponse->body->items;
+            $push = array(
+                "id" => $book->id,
+                "isbn" => $book->isbn,
+                "details" => $resp[0]->volumeInfo);
             array_push($response, $push);
 
         }
-        return Response::json(["books" => $response],200);
+        return Response::json($response,200);
 
 	}
 
